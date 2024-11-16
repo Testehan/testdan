5320fda1d2bdac31f4fd9d2369b893d2a18f3a90
DanTestehan <tdan89@yahoo.com>
2024-11-16 20:20:20 +0100

    CV-dante - initial App files

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/homePage';
+import QuotesPage from './pages/quotesPage';
+
+function App() {
+
+
+  return (
+    <>
+      <Router>
+        {/* right now i just want different pages, and the quotes page does not have a menu...i could at some point use the navbar from main
+         page and when a user clicks on the Quotes there to take him to the Quotes page not the quotes section*/}
+        {/* Navbar */}
+        {/*<nav className="bg-blue-600 text-white p-4">*/}
+        {/*  <ul className="flex justify-center space-x-6">*/}
+        {/*    <li>*/}
+        {/*      <a href="/" className="hover:underline">Home</a>*/}
+        {/*    </li>*/}
+        {/*    <li>*/}
+        {/*      <a href="/quotes" className="hover:underline">Quotes</a>*/}
+        {/*    </li>*/}
+        {/*  </ul>*/}
+        {/*</nav>*/}
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/quotes" element={<QuotesPage />} />
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
---
+++ src/joi-browser.d.ts
@@ -0,0 +1,4 @@
+declare module 'joi-browser' {
+    import Joi from 'joi'; // Import the main Joi types
+    export default Joi;   // Export Joi as the default export
+}
