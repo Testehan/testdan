971baa3e3cd12f4eb19239912ea12e1fe7f880cf
DanTestehan <tdan89@yahoo.com>
2024-11-27 20:20:20 +0100

    CV-dante - added quotes page

---
+++ src/pages/quotesPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import QuotesSection from "../components/quotes.tsx";
+
+const QuotesPage: React.FC = () => {
+    return (
+        <QuotesSection />
+    );
+};
+
+export default QuotesPage;
