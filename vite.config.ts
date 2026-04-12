19138d2433c7c255cfcecff53580ff309f26d0b4
DanTestehan <tdan89@yahoo.com>
2026-04-12 11:21:47 +0300

    Fix env var reading for production

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '', [])
-  
-  return {
-    plugins: [react()],
-    define: {
-      'import.meta.env.VITE_NEXTSTEP_URL': JSON.stringify(env.VITE_NEXTSTEP_URL || 'http://localhost:8084'),
-      'import.meta.env.VITE_NEXTSTEP_API_SECRET_KEY': JSON.stringify(env.VITE_NEXTSTEP_API_SECRET_KEY || ''),
-    },
-  }
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    'import.meta.env.VITE_NEXTSTEP_URL': JSON.stringify(process.env.VITE_NEXTSTEP_URL || 'http://localhost:8084'),
+    'import.meta.env.VITE_NEXTSTEP_API_SECRET_KEY': JSON.stringify(process.env.VITE_NEXTSTEP_API_SECRET_KEY || ''),
+  },
 })
